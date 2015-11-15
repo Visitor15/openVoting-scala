08917dbf517af1afd87ed3389c1defcc0649e76e
visitor15 <visitor15@gmail.com>
2015-11-15 10:11:14 -0500

    Initial hello world commit.

---
+++ src/main/scala/com/forged/server/RootActor.scala
@@ -0,0 +1,25 @@
+package com.forged.server
+
+import spray.http.MediaTypes._
+import spray.routing.HttpServiceActor
+
+/**
+ * Created by visitor15 on 11/15/15.
+ */
+class RootActor extends HttpServiceActor {
+  override def receive: Receive = runRoute {
+    path("") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Hello, world!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    }
+  }
+}
